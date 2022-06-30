5494a04d67dac4e74c8debcba5bc39fed1756fc2
unknown <Jack Shoemaker>
2022-06-30 00:19:11 -0700

    Updated movement to include momentum, could clean up

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,3 +1,4 @@
+from os import curdir
 import tkinter as tk
 import threading 
 import multiprocessing
@@ -7,16 +8,18 @@ import random
 #y bound 0-580
 class SnakeGame():
     currMomentum = "none"
+    oppositeDir = [("Up", "Down"), ("Right", "Left"), ("Down", "Up"), ("Left", "Right")]
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Snake Game")
         self.window.configure(bg = "black")
         self.window.geometry("600x600")
         self.window.resizable(False, False)
-        self.window.bind("<Left>", self.momentum)
-        self.window.bind("<Right>", self.momentum)
-        self.window.bind("<Up>", self.momentum)
-        self.window.bind("<Down>", self.momentum)
+
+        self.window.bind("<Left>", self.changeMomentum)
+        self.window.bind("<Right>", self.changeMomentum)
+        self.window.bind("<Up>", self.changeMomentum)
+        self.window.bind("<Down>", self.changeMomentum)
         self.makeArea(self)
         #need to add after for momentum and movement 
         self.window.mainloop()
@@ -30,31 +33,49 @@ class SnakeGame():
         self.apple = tk.Frame(self.window, bg ="red", width = 20, height =20)
         self.apple.place(x = 250, y =150)
     
-    def left(self, event):
+    def left(self):
         if(self.snake.winfo_x() - 10 <= 0):
             self.endGame(self)
         else: 
-            self.snake.place(x= self.snake.winfo_x() - 10)
+            self.snake.place(x= self.snake.winfo_x() - 5)
 
-    def right (self, event):
-        if(self.snake.winfo_x() + 10 >= 580):
+    def right (self):
+        if(self.snake.winfo_x() + 10 >= 570):
             self.endGame(self)
         else:
-            self.snake.place(x= self.snake.winfo_x() + 10)
+            self.snake.place(x= self.snake.winfo_x() + 5)
 
-    def up(self,event):
+    def up(self):
         if(self.snake.winfo_y() - 10 <= 0):
             self.endGame(self)
         else:
-            self.snake.place(y= self.snake.winfo_y() - 10)
+            self.snake.place(y= self.snake.winfo_y() - 5)
 
-    def down(self,event):
+    def down(self):
         if(self.snake.winfo_y() + 10 >= 570):
             self.endGame(self)
         else:
-            self.snake.place(y= self.snake.winfo_y() + 10)
-    def momentum(self, event):
-        pass 
+            self.snake.place(y= self.snake.winfo_y() + 5)
+    #contiunously checks for input change
+    def changeMomentum(self, event):
+        if(self.currMomentum == "none"):
+            self.currMomentum = event.keysym
+            self.momentum()
+        elif(self.currMomentum != event.keysym and self.oppositeDir.count((event.keysym, self.currMomentum))==0):
+            self.currMomentum = event.keysym
+        else:
+            pass
+
+    def momentum(self):
+        if(self.currMomentum == "Left"):
+            self.left()
+        elif(self.currMomentum == "Right"):
+            self.right()
+        elif(self.currMomentum == "Up"):
+            self.up()
+        elif(self.currMomentum == "Down"):
+            self.down()
+        self.window.after(20, self.momentum)
     def endGame(self, event):
         pass
 run = SnakeGame()
